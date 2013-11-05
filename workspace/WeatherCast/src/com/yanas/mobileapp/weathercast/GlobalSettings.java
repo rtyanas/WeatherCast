1ff6e78116d88920c208c79b5f1b8303fc4f7503
rtyanas <rtyanas2000@yahoo.com>
2013-11-04 23:07:41 -0500

    Initial version controls the logging using global boolean in if statements

---
+++ workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/GlobalSettings.java
@@ -0,0 +1,14 @@
+package com.yanas.mobileapp.weathercast;
+
+public class GlobalSettings {
+
+	// weathercast package
+	
+	public static boolean weatherData = true;
+	public static boolean stationData = true;
+
+	// parsexml
+	public static boolean weatherXmlParsing = true;
+	public static boolean weatherDataParsed = true;
+
+}
