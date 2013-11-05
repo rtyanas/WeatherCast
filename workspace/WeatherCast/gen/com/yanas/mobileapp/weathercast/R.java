07299cb752ab6e7160881d819c0be1f3ea294399
rtyanas <rtyanas2000@yahoo.com>
2013-11-04 23:47:46 -0500

    Added resources for new layout

--- workspace/WeatherCast/gen/com/yanas/mobileapp/weathercast/R.java
+++ workspace/WeatherCast/gen/com/yanas/mobileapp/weathercast/R.java
@@ -21,19 +21,37 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int app_icon=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int moon_cloud=0x7f020003;
+        public static final int sun_cloud=0x7f020004;
+        public static final int temp_guage=0x7f020005;
     }
     public static final class id {
-        public static final int action_search=0x7f080008;
-        public static final int action_settings=0x7f080007;
-        public static final int content=0x7f080004;
-        public static final int icon=0x7f080005;
-        public static final int label=0x7f080006;
+        public static final int action_search=0x7f080016;
+        public static final int action_settings=0x7f080015;
+        public static final int content=0x7f080012;
+        public static final int date_time=0x7f080004;
+        public static final int icon=0x7f080013;
+        public static final int imageView1=0x7f080009;
+        public static final int label=0x7f080014;
         public static final int listView=0x7f080003;
         public static final int listview=0x7f080001;
         public static final int pager=0x7f080000;
+        public static final int pop=0x7f08000e;
+        public static final int pop_image=0x7f08000d;
         public static final int station_id=0x7f080002;
+        public static final int temp_hour=0x7f080006;
+        public static final int temp_icon=0x7f080005;
+        public static final int temp_max=0x7f080008;
+        public static final int temp_min=0x7f080007;
+        public static final int weather_predominant=0x7f08000f;
+        public static final int weather_predominant_amount=0x7f080010;
+        public static final int weather_predominant_rain=0x7f080011;
+        public static final int wind_direction=0x7f08000b;
+        public static final int wind_gust=0x7f08000c;
+        public static final int wind_sustained=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -42,8 +60,9 @@ public final class R {
         public static final int activity_main=0x7f030003;
         public static final int activity_station_list=0x7f030004;
         public static final int custom_list_activity_view=0x7f030005;
-        public static final int fragment2_screen_slide_page=0x7f030006;
-        public static final int rowlayout=0x7f030007;
+        public static final int display_weather_layout=0x7f030006;
+        public static final int fragment2_screen_slide_page=0x7f030007;
+        public static final int rowlayout=0x7f030008;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
@@ -57,10 +76,14 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int pop=0x7f05000b;
         public static final int station_id=0x7f050003;
+        public static final int temperature=0x7f050009;
         public static final int title_activity_display_message=0x7f050005;
         public static final int title_activity_station_list=0x7f050007;
         public static final int title_template_step=0x7f050008;
+        public static final int weather=0x7f05000c;
+        public static final int wind=0x7f05000a;
     }
     public static final class style {
         /** 
