e54fed506d3e9ce99263bd2d42a96ee4ce2ae819
rtyanas <rtyanas2000@yahoo.com>
2013-11-15 18:23:22 -0500

    CHange MainActivity to StationList

--- workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/MainActivity.java
+++ workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/MainActivity.java
@@ -1,166 +1,144 @@
 package com.yanas.mobileapp.weathercast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Vector;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
-import com.yanas.mobileapp.restaccess.RestClient;
-import com.yanas.mobileapp.restaccess.RestClient.RequestMethod;
-import com.yanas.mobileapp.weathercast.parsexml.WeatherDataParsed;
-import com.yanas.mobileapp.weathercast.parsexml.WeatherXmlParsing;
-import com.yanas.mobileapp.weathercast.parsexml.WeatherDataParsed.DisplayData;
-import com.yanas.mobileapp.weathercast.parsexml.WeatherDataParsed.LayoutAndDates;
-
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.app.Activity;
+import android.annotation.TargetApi;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity         
+/* implements LoaderManager.LoaderCallbacks<Cursor> */ {
 
-	SimpleAdapter simpleAdpt;
+    // This is the Adapter being used to display the list's data
+    SimpleCursorAdapter mAdapter;
+
+	public final static String LOCATION_ID = "com.yanas.mobileapp.weathercast.CURRENT_LOCATION";
+
+	public final static String CURRENT_LOCATION = "XX_Current_Location_YY";
 	
-	public final static String STATION_ID = "com.yanas.mobileapp.weathercast.STATION_ENTERED";
-	
+    Vector<StationSelected> stations;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_station_list);
 
+		setupActionBar();
+		initStationData(this);
+
+		ArrayList<String> stationsAL = new ArrayList<String>();
 		
-//        Cursor mCursor = this.getContentResolver().query(People.CONTENT_URI, null, null, null, null);
-//        startManagingCursor(mCursor);
-//
-//        // Now create a new list adapter bound to the cursor.
-//        // SimpleListAdapter is designed for binding to a Cursor.
-//        ListAdapter adapter = new SimpleCursorAdapter(
-//                this, // Context.
-//                android.R.layout.two_line_list_item,  // Specify the row template to use (here, two columns bound to the two retrieved cursor rows).
-//                mCursor,                                              // Pass in the cursor to bind to.
-//                new String[] {People.NAME, People.NUMBER},           // Array of cursor columns to bind to.
-//                new int[] {android.R.id.text1, android.R.id.text2});  // Parallel array of which template objects to bind to those columns.
-//
-//        // Bind to our new adapter.
-//        setListAdapter(adapter);
-
-        
-		 setContentView(R.layout.activity_main);
-		// getActionBar().setDisplayHomeAsUpEnabled(true);
-	    initList();
-
-	    // We get the ListView component from the layout
-	    ListView lv = (ListView) findViewById(R.id.listView);
-
-	    // This is a simple adapter that accepts as parameter
-	    // Context
-	    // Data list
-	    // The row layout that is used during the row creation
-	    // The keys used to retrieve the data
-	    // The View id used to show the data. The key number and the view id must match
-	    simpleAdpt = new SimpleAdapter(this, planetsList, android.R.layout.simple_list_item_1, new String[] {"planet"}, new int[] {android.R.id.text1});
-
-	    
-	    /* Test Code
-   		String xmlFileOut = "weather.xml";
-    	File path = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOWNLOADS);
-    	File file = new File(path, xmlFileOut);
-    	Log.d("AminActivity", "Output file: "+ path +"/"+ xmlFileOut );
-    	
-		FileInputStream fis = null;
-	    try {
-			fis = new FileInputStream(file); // "C:/Users/RT/Weather/messasgeExample.xml");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(StationSelected sd : stations) {
+			stationsAL.add(sd.getCity() +", "+ sd.getState() +", "+ sd.getZipCode() );
 		}
-
-	    WeatherXmlParsing wxp = new WeatherXmlParsing (fis);
-	    WeatherDataParsed wdp =  wxp.parse();
-	    wdp.updateWeatherDataPeriod();
-	    
-	    DisplayData dd = wdp.new DisplayData();
-	    List<DisplayData> ddL = dd.generateDisplayDataList(wdp.stationData);
-	    */
-
-	    // lv.setAdapter(simpleAdpt);
-
-		Intent intent = new Intent(this, StationListActivity.class);
-		startActivity(intent);
+		
+        setListAdapter(new StableArrayAdapter(this, 
+        		android.R.layout.simple_list_item_1, stationsAL) );
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.station_list, menu);
 		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
 		return true;
 	}
+
 	
-	public void sendMessage(View view) {
-		
-		Intent intent = new Intent(this, StationListActivity.class);
-		
-		// Intent intent = new Intent(this, ListViewExampleActivity.class);
-		// Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText textMessage = (EditText) findViewById(R.id.station_id);
-		String station = textMessage.getText().toString();
-		intent.putExtra(STATION_ID, station);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+    @Override 
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        final String item = (String) l.getItemAtPosition(position);
+        Log.d("OnClick", "item: "+ item);
+        
+		Intent intent = new Intent(this, DisplayWeatherInfoActivity.class);
+		intent.putExtra(LOCATION_ID, stations.get(position).getStationData());
 		startActivity(intent);
+    }
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
+
+	private void initStationData(MainActivity this_in) {
+		stations = new Vector<StationSelected>();
+		stations.add(new StationSelected(this_in, CURRENT_LOCATION, "", ""));
+		stations.add(new StationSelected(this_in, "Bedminster", "NJ", "07921"));
+		stations.add(new StationSelected(this_in, "Flemington", "NJ", "08822"));
+		stations.add(new StationSelected(this_in, "Fair Haven", "NJ", "07704"));
+		stations.add(new StationSelected(this_in, "Mantoloking", "NJ", "08738"));
+		stations.add(new StationSelected(this_in, "Netcong", "NJ", "07857"));
+		stations.add(new StationSelected(this_in, "New York", "NY", "10018"));
+		stations.add(new StationSelected(this_in, "Perth Amboy", "NJ", "08861"));
+		stations.add(new StationSelected(this_in, "Rock Hall", "MD", "21661"));
+		stations.add(new StationSelected(this_in, "Rockaway", "NJ", "07866"));
+		stations.add(new StationSelected(this_in, "Sandy Hook", "NJ", "07732"));
+		stations.add(new StationSelected(this_in, "Shrewsbury", "NJ", "07702"));
+		stations.add(new StationSelected(this_in, "Toms River", "NJ", "08722"));
+	}
+
 	
-	// The data to show
-	List<Map<String, String>> planetsList = new ArrayList<Map<String,String>>();
+	/**
+	 * Create the Array
+	 * @author RT
+	 *
+	 */
+    private class StableArrayAdapter extends ArrayAdapter<String> {
 
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 
-	   private void initList() {
-	    // We populate the planets
+        public StableArrayAdapter(Context context, int textViewResourceId,
+            List<String> objects) {
+          super(context, textViewResourceId, objects);
+          for (int i = 0; i < objects.size(); ++i) {
+            mIdMap.put(objects.get(i), i);
+          }
+        }
 
-	    planetsList.add(createPlanet("planet", "Mercury"));
-	    planetsList.add(createPlanet("planet", "Venus"));
-	    planetsList.add(createPlanet("planet", "Mars"));
-	    planetsList.add(createPlanet("planet", "Jupiter"));
-	    planetsList.add(createPlanet("planet", "Saturn"));
-	    planetsList.add(createPlanet("planet", "Uranus"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Neptune"));
-	    planetsList.add(createPlanet("planet", "Mars"));
-	    planetsList.add(createPlanet("planet", "Jupiter"));
-	    planetsList.add(createPlanet("planet", "Earth"));
+        @Override
+        public long getItemId(int position) {
+          String item = getItem(position);
+          return mIdMap.get(item);
+        }
 
-	}
-
-	private HashMap<String, String> createPlanet(String key, String name) {
-	    HashMap<String, String> planet = new HashMap<String, String>();
-	    planet.put(key, name);
-
-	    return planet;
-	}
+        @Override
+        public boolean hasStableIds() {
+          return true;
+        }
+    }
+    
 
 }
