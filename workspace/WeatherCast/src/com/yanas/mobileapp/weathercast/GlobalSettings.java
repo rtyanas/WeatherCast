a850472b942a042c52657f6d31f6c8ac897dd161
rtyanas <rtyanas2000@yahoo.com>
2013-11-29 13:44:27 -0500

    Add city_list_db_data

--- workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/GlobalSettings.java
+++ workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/GlobalSettings.java
@@ -2,13 +2,15 @@ package com.yanas.mobileapp.weathercast;
 
 public class GlobalSettings {
 
+	public static boolean city_list_db_data = true;	
+	
 	// weathercast package
 	
-	public static boolean weatherData = true;
-	public static boolean stationData = true;
+	public static boolean weatherData = false;
+	public static boolean stationData = false;
 
 	// parsexml
-	public static boolean weatherXmlParsing = true;
+	public static boolean weatherXmlParsing = false;
 	public static boolean weatherDataParsed = true;
 
 	public static boolean display_weatherInfo_access_pagefrag = true;
