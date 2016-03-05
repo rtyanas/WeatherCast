49f83391800a45c69c3d7bf4258e04b435a8b8cc
rtyanas <rtyanas2000@yahoo.com>
2016-03-04 21:34:52 -0500

    add setting access and DB updates

--- workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/SettingsWeather.java
+++ workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/SettingsWeather.java
@@ -1,17 +1,80 @@
 package com.yanas.mobileapp.weathercast;
 
+import android.database.DatabaseUtils;
+import android.util.Log;
+
+import com.yanas.mobileapp.weathercast.datastore.CityListDbHelper;
+import com.yanas.mobileapp.weathercast.datastore.UserSettingsDbData;
+import com.yanas.mobileapp.weathercast.datastore.UserSettingsDbHelper;
+
 public class SettingsWeather {
     
     public enum PanelSelectionEnum  {
-        SINGLE_PANE, QUAD_PANE_ONLY_MIN_GRAPHICS, QUAD_PANE_ONLY_GRAPHICS, 
-        QUAD_PANE_ONLY_NUMBERS
+        SINGLE_PANE(0), QUAD_PANE_ONLY_MIN_GRAPHICS(1), QUAD_PANE_ONLY_GRAPHICS(2), 
+        QUAD_PANE_ONLY_NUMBERS(3);
+        private int value;
+        private PanelSelectionEnum(int val) {
+            value = val;
+        }
+        public int getValue() {
+            return value;
+        }
     };
 
-    public PanelSelectionEnum panelSelect;
+    private int id;
+    private PanelSelectionEnum panelSelect;
+    private int backgroundColor;
+    private int cityStateListSort;
+    UserSettingsDbData userSettingsDbData;
+    private long countDbReturn;
 
+    public SettingsWeather(UserSettingsDbData userSettingsDbData_in) {
+        userSettingsDbData = userSettingsDbData_in;
+        init();
+    }
+    
     public SettingsWeather() {
+        userSettingsDbData = null;
+        init();
+    }
+    
+    private boolean init() {
         
-        panelSelect = PanelSelectionEnum.QUAD_PANE_ONLY_MIN_GRAPHICS ;
+        // Default values
+        panelSelect = PanelSelectionEnum.QUAD_PANE_ONLY_NUMBERS;
+        backgroundColor = 0;
+        cityStateListSort = 0;
+        return true;
+    }
+    
+    public boolean getSettingsWeather() {
+
+        SettingsWeather sw = userSettingsDbData.getSavedSettings();
+        if(sw.getCountDbReturn() > 0) {
+            panelSelect = sw.getPanelSelect();
+            backgroundColor = sw.getBackgroundColor();
+            cityStateListSort = sw.getCityStateListSort();
+            if(GlobalSettings.main_activity) Log.d(SettingsWeather.class.getName(), "Found DB values found and loaded");
+        }
+        else {
+            if(GlobalSettings.main_activity) Log.w(SettingsWeather.class.getName(), "DB values not found");
+            init();
+            userSettingsDbData.insertSetting(this);
+        }
+
+        return true;
+    }
+    
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public long saveSettingsWeather() {
+        return userSettingsDbData.updateSetting(this);
         
     }
 
@@ -19,7 +82,54 @@ public class SettingsWeather {
         return panelSelect;
     }
 
+    public int getPanelSelectInt() {
+        return panelSelect.getValue();
+    }
+
     public void setPanelSelect(PanelSelectionEnum panelSelect) {
         this.panelSelect = panelSelect;
     }
+    
+    public void setPanelSelect(int panelSelect) {
+        this.panelSelect = PanelSelectionEnum.QUAD_PANE_ONLY_NUMBERS;
+        
+        if(panelSelect == PanelSelectionEnum.QUAD_PANE_ONLY_NUMBERS.getValue() ) {
+            this.panelSelect = PanelSelectionEnum.QUAD_PANE_ONLY_NUMBERS;
+        } 
+        else if(panelSelect == PanelSelectionEnum.QUAD_PANE_ONLY_GRAPHICS.getValue() ) {
+            this.panelSelect = PanelSelectionEnum.QUAD_PANE_ONLY_GRAPHICS;
+        }
+        else if(panelSelect == PanelSelectionEnum.QUAD_PANE_ONLY_MIN_GRAPHICS.getValue() ) {
+            this.panelSelect = PanelSelectionEnum.QUAD_PANE_ONLY_MIN_GRAPHICS;
+        }
+        else if(panelSelect == PanelSelectionEnum.SINGLE_PANE.getValue() ) {
+            this.panelSelect = PanelSelectionEnum.SINGLE_PANE;
+        }
+    }
+    
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public int getCityStateListSort() {
+        return cityStateListSort;
+    }
+
+    public void setCityStateListSort(int cityStateListSort) {
+        this.cityStateListSort = cityStateListSort;
+    }
+
+    public long getCountDbReturn() {
+        return countDbReturn;
+    }
+
+    public void setCountDbReturn(long countDbReturn) {
+        this.countDbReturn = countDbReturn;
+    }
+
+    
 }
