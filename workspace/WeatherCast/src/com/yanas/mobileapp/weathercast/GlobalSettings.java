c822beabaadaf9cc696cdded547fad4c66157c5c
rtyanas <rtyanas2000@yahoo.com>
2016-03-15 08:04:13 -0400

    update

--- workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/GlobalSettings.java
+++ workspace/WeatherCast/src/com/yanas/mobileapp/weathercast/GlobalSettings.java
@@ -6,12 +6,14 @@ public class GlobalSettings {
 	
 	// weathercast package
 	
-	public static boolean weatherData = false;
-	public static boolean stationData = false;
+	public static boolean weatherData = true;
+	public static boolean stationData = true;
 
 	// parsexml
-	public static boolean weatherXmlParsing = false;
+	public static boolean weatherXmlParsing = true;
 	public static boolean weatherDataParsed = true;
+	
+    public static boolean backgroundActivity = true;
 
 	public static boolean display_weatherInfo_access_pagefrag = true;
 	
